3177cb500ba3cd82d034f155223ec0d4bea2dd12
chrisnunta <koapisith@gmail.com>
2017-05-10 15:43:42 +0700

    Service menu

--- app/src/main/java/com/carmemorize/app/activity/Service.java
+++ app/src/main/java/com/carmemorize/app/activity/Service.java
@@ -19,7 +19,7 @@ public class Service extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service_show);
+        setContentView(R.layout.activity_service);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
